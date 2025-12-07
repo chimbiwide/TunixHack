bc2fae617f9bdba8965310fe29e7d4885905828a
david <david@A.PC>
2025-12-07 18:01:58 -0500

    added time count in generation process

--- generate_databricks.py
+++ generate_databricks.py
@@ -2,6 +2,7 @@ import lmstudio as lms
 import json
 import re
 import csv
+import time
 from prompts import PROMPTS
 
 def process_file(filename: str):
@@ -41,13 +42,29 @@ def generate_responses(data:list[str]):
     model = lms.llm("qwen3-14b")
     data_length = len(data)
     llm_response = []
+    start_time = time.time()
     for i, prompt in enumerate(data,1):
        response = replace_thinking(model.respond(prompt).content).strip()
-       print(f"Generated {i}/{data_length} responses")
+       print_status(start_time, i, data_length)
        llm_response.append(response)
-       if (i == 10):
-           break
+
+    total_time = time.time() - start_time
+    print(f"\n\n\n Total Generation Time: {total_time/60:.2f} minutes")
     return llm_response
+
+def print_status(start_time, i, data_length):
+   elapsed_time = time.time() - start_time
+
+   days = int(elapsed_time // 86400)
+   hours = int((elapsed_time % 86400) // 3600)
+   mins = int((elapsed_time % 3600) // 60)
+   secs = int(elapsed_time % 60)
+   if days > 0:
+       status = f"{days:02d}:{hours:02d}:{mins:02d}:{secs:02d}"
+   else:
+       status = f"{hours:02d}:{mins:02d}:{secs:02d}"
+   print(f"Generated {i}/{data_length} responses | Elapsed: {status}")
+
 
 def replace_thinking(response:str):
     return re.sub(r'<think>.*?</think>', '', response, flags=re.DOTALL)
