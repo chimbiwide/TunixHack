b9f19d7184b1a469fd4e1b5d0400e737b49e5cf9
david <david@A.PC>
2025-12-07 17:05:12 -0500

    finished generation script for databricks dataset

--- generate_databricks.py
+++ generate_databricks.py
@@ -1,20 +1,64 @@
 import lmstudio as lms
 import json
+import re
+import csv
+from prompts import PROMPTS
 
 def process_file(filename: str):
     data = []
     with open(filename, 'r') as f:
         for line in f:
-            line.strip()
-            currectLine = json.loads(line)
-            system_prompt =
+            line = line.strip()
+            currentLine = json.loads(line)
+            current_sys_prompt = create_system_prompt(currentLine)
+            data.append(current_sys_prompt)
+    return data
 
 def create_system_prompt(currentLine):
     prompt = currentLine.get("instruction")
     context = currentLine.get("context")
     response = currentLine.get("response")
     category = currentLine.get("category")
+    sys_prompt = PROMPTS.get(category) + "Prompt: " + prompt
+    match category:
+        case "summarization":
+            sys_prompt += "\nContext: " + context
+        case "closed_qa":
+            sys_prompt += "\nContext: " + context
+        case "open_qa":
+            sys_prompt += "\nCorrect Answer: " + response
+        case "information_extraction":
+            sys_prompt += "\nSupplementary Text: " + context
+        case "classification":
+            sys_prompt += "\nCorrect Answer: " + response
+    return sys_prompt
 
-    System_prompt_template = f"""
-    
-    """
+def generate_responses(data:list[str]):
+    model = lms.llm("qwen3-14b")
+    data_length = len(data)
+    llm_response = []
+    for i, prompt in enumerate(data,1):
+       response = replace_thinking(model.respond(prompt).content).strip()
+       print(f"Generated {i}/{data_length} responses")
+       llm_response.append(response)
+       if (i == 10):
+           break
+    return llm_response
+
+def replace_thinking(response:str):
+    return re.sub(r'<think>.*?</think>', '', response, flags=re.DOTALL)
+
+def write_csv(prompt:list[str], response:list[str], outputfilename:str):
+    with open(outputfilename, 'w', newline='')  as f:
+        writer = csv.writer(f)
+        header = ["Prompt", "Model_Response"]
+        writer.writerow(header)
+        writer.writerows(zip(prompt, response))
+
+def main():
+    data = process_file("databricks.jsonl")
+    responses = generate_responses(data)
+    write_csv(data, responses, "databricks_with_reason.csv")
+
+if __name__ == "__main__":
+    main()
--- prompts.py
+++ prompts.py
@@ -1,17 +1,20 @@
+#dont need context
 creative_writing = """
 You will be given a creative writing prompt. Generate the reasoning traces that get to the final response. 
 Your reasoning should logically lead to your final response, think about how you will structure the reasoning traces.
 Your final answer should follow this format:
 <reasoning>reasoning goes here</reasoning><answer>answer goes in here</answer>
-"""
 
+"""
+# need context
 closed_qa = """
 You will be given a question, and a passage of relevant information. Generate the reasoning traces that get to the final answer.
 Your reasoning should logically lead to your final answer, think about how you will structure the reasoning traces.
 Your final answer should follow this format:
 <reasoning>reasoning goes here</reasoning><answer>answer goes in here</answer>
-"""
 
+"""
+# need context
 open_qa = """
 You will be given a question, and a correct answer. Generate the reasoning traces that get to the final answer.
 Your reasoning should logically lead to your final answer, think about how you will structure the reasoning traces.
@@ -19,12 +22,63 @@ Do not reference that the correct answer was provided in the reasoning traces.
 You can paraphrase the answer and add additional information.
 Your final answer should follow this format:
 <reasoning>reasoning goes here</reasoning><answer>answer goes in here</answer>
-"""
 
+"""
+# need context
 summarization = """
 You will be given a question, and a paragraph you must summarize the answer from.
 Generate the reasoning traces that get to the final answer.
 Your reasoning should logically lead to your final response, think about how you will structure the reasoning traces.
 Your final answer should follow this format:
 <reasoning>reasoning goes here</reasoning><answer>answer goes in here</answer>
+
 """
+# need context
+information_extraction = """
+You will be given a question, and a supplementary text. Your answer should be based on the text provided.
+Generate the reasoning traces that get to the final answer.
+Your reasoning should logically lead to your final response, think about how you will structure the reasoning traces.
+Your final answer should follow this format:
+<reasoning>reasoning goes here</reasoning><answer>answer goes in here</answer>
+
+"""
+
+#need context
+classification = """
+You will be given a classification question and the correct answer. Generate the reasoning traces that get to the final answer.
+Your reasoning should logically lead to your final response, think about how you will structure the reasoning traces.
+Do not reference that the correct answer was provided in the reasoning traces.
+Your final answer should follow this format:
+<reasoning>reasoning goes here</reasoning><answer>answer goes in here</answer>
+
+"""
+
+#dont need context
+brainstorming = """
+You will be given a brainstorming idea. Generate the reasoning traces that get to the final answer.
+your reasoning should logically lead to your final response, think about how you will structure the reasoning traces.
+your final final answer should follow this format:
+<reasoning>reasoning goes here</reasoning><answer>answer goes in here</answer>
+
+"""
+
+#dont need context
+general_qa = """
+You will be given a open-ended question. Generate the reasoning traces that get to the final answer.
+your reasoning should logically lead to your final response, think about how you will structure the reasoning traces.
+your final final answer should follow this format:
+<reasoning>reasoning goes here</reasoning><answer>answer goes in here</answer>
+
+"""
+
+# Dictionary mapping category names to prompts
+PROMPTS = {
+    "creative_writing": creative_writing,
+    "closed_qa": closed_qa,
+    "open_qa": open_qa,
+    "summarization": summarization,
+    "information_extraction": information_extraction,
+    "classification": classification,
+    "brainstorming": brainstorming,
+    "general_qa": general_qa,
+}
