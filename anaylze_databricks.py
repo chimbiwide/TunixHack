e4c6f6238b283fa84d9b35eef2ee41574e119907
chimbiwide <chimbiwide@A.pc>
2025-12-14 10:15:56 -0500

    added filtering script

--- anaylze_databricks.py
+++ anaylze_databricks.py
@@ -1,4 +1,7 @@
 import csv
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-3-1b-it")
 
 def count_categories(filename:str):
     categories = {
@@ -18,13 +21,41 @@ def count_categories(filename:str):
 
     return categories
 
-def write_csv(categories: dict, output:str):
+def count_tokens(txt:str) -> int:
+    return len(tokenizer.encode(txt))
+
+def in_limit(txt:str, max_tokens:int=1024) -> bool:
+    return count_tokens(txt) <= max_tokens
+
+
+def filter_csv(input:str, output:str, max_tokens:int=1024):
+    with open(input, 'r') as in_f, open(output, 'w', newline='') as out_f:
+        reader = csv.DictReader(in_f)
+        writer  = csv.DictWriter(out_f,fieldnames=reader.fieldnames)
+        writer.writeheader()
+
+        total = 0
+        kept = 0
+
+        for row in reader:
+            total += 1
+            
+            target_text = row["Prompt"] + row["Model_Response"]
+            
+            if in_limit(target_text, max_tokens=max_tokens):
+                writer.writerow(row)
+                kept += 1
+
+        print(f"Kept {kept}/{total} conversations")
+
+
+def write_categories(categories: dict, output:str):
     fieldnames = ['open_qa', 
                   'general_qa', 
                   'classification',
                   'closed_qa',
                   'brainstorming',
-                  'infomation_extraction',
+                  'information_extraction',
                   'summarization',
                   'creative_writing'
                   ]
@@ -32,13 +63,13 @@ def write_csv(categories: dict, output:str):
         writer = csv.DictWriter(f, fieldnames=fieldnames)
 
         writer.writeheader()
-        writer.writerows(categories)
+        writer.writerow(categories)
         print("wrote_file")
 
 def main():
     cat_count = count_categories("databricks_with_reason.csv")
     print(cat_count)
-    #write_csv(cat_count, "categories_count.csv")
+    write_categories(cat_count, "categories_count.csv")
 
 if __name__ == "__main__":
-    main()
+    filter_csv("databricks_with_reason.csv", "databricks_filtered_1024.csv", 1024)
